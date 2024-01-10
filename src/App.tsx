52cb4a864547030a6347673430616cda90a4e540
Vladimirs Pavlicenko <vladimirs@pavlicenko.com>
2024-01-10 10:14:48 +0000

    display table with refund request approval

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,142 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {parse, format, isAfter, addHours, isBefore} from 'date-fns'
+import { reversalRequestsJSON } from './reversalRequests';
+
+import {
+  Table,
+  Header,
+  HeaderRow,
+  Body,
+  Row,
+  HeaderCell,
+  Cell,
+} from "@table-library/react-table-library/table";
+
+import { useTheme } from "@table-library/react-table-library/theme";
+import { getTheme } from "@table-library/react-table-library/baseline";
+
+type CustomerLocation = 'US' | 'Europe';
+type RequestSource = 'web app' | 'phone';
+
+interface RawReversalRequest {
+  name: string;
+  customerLocation: CustomerLocation;
+  signUpDate: string;
+  requestSource: RequestSource;
+  investmentDate: string;
+  investmentTime: string;
+  refundRequestDate: string;
+  refundRequestTime: string;
+}
+
+interface ReversalRequest  {
+  name: string;
+  customerLocation: CustomerLocation;
+  signUpDate: Date;
+  investmentDateTime: Date;
+  refundRequestDateTime: Date;
+  requestSource: RequestSource;
+  isSubjectToNewTos: boolean;
+  isReversalRequestApproved: boolean;
+}
+
+const US_DATE_FORMAT = 'M/d/yyyy'; 
+const EUROPE_DATE_FORMAT = 'd/M/yyyy';
+const NEW_TOC_CUT_OFF_DATE_EU = '2/1/2020';
 
 function App() {
+
+  const getDateTime = (dateSting: string, location: CustomerLocation) => {
+    return location === 'Europe' 
+      ? parse(dateSting, `${EUROPE_DATE_FORMAT} H:m`, new Date())
+      : parse(dateSting, `${US_DATE_FORMAT} H:m`, new Date());
+  };
+
+  const getDate = (dateSting: string, location: CustomerLocation) => {
+    return location === 'Europe' 
+      ? parse(dateSting, EUROPE_DATE_FORMAT, new Date())
+      : parse(dateSting, US_DATE_FORMAT, new Date());
+  };
+
+  const getRefundRequestApproval = (reversalRequest: Omit<ReversalRequest, 'isReversalRequestApproved'>): boolean => {
+    const {isSubjectToNewTos, investmentDateTime, refundRequestDateTime, requestSource  } = reversalRequest;
+    switch (requestSource) {
+      case 'phone': {
+        return isSubjectToNewTos 
+          ? isBefore(refundRequestDateTime, addHours(investmentDateTime, 8))
+          : isBefore(refundRequestDateTime, addHours(investmentDateTime, 4));
+      }
+      case 'web app': {
+        return isSubjectToNewTos 
+          ? isBefore(refundRequestDateTime, addHours(investmentDateTime, 16))
+          : isBefore(refundRequestDateTime, addHours(investmentDateTime, 8));
+      }
+      default: return false
+    }
+  }
+
+  const reversalRequests: ReversalRequest[] = JSON.parse(reversalRequestsJSON)
+    .map((reversalRequest: RawReversalRequest)=> {
+      const { name,
+        customerLocation,
+        signUpDate,
+        requestSource,
+        investmentDate,
+        investmentTime,
+        refundRequestDate,
+        refundRequestTime} = reversalRequest
+      return {
+        name,
+        customerLocation,
+        signUpDate: getDate(signUpDate, customerLocation),
+        investmentDateTime: getDateTime(`${investmentDate} ${investmentTime}`, customerLocation),
+        refundRequestDateTime: getDateTime(`${refundRequestDate} ${refundRequestTime}`, customerLocation),
+        requestSource
+      }
+    }).map((reversalRequest: Omit<ReversalRequest, 'isSubjectToNewTos' | 'isReversalRequestApproved'>) => ({
+      ...reversalRequest,
+      isSubjectToNewTos: isAfter(reversalRequest.signUpDate, getDate(NEW_TOC_CUT_OFF_DATE_EU, 'Europe'))
+    })).map((reversalRequest: Omit<ReversalRequest, 'isReversalRequestApproved'>) => ({
+      ...reversalRequest,
+      isReversalRequestApproved: getRefundRequestApproval(reversalRequest)
+    }));
+
+    const tableData = {
+      nodes: reversalRequests.map((reversalRequest, index) => ({...reversalRequest, id: index}))
+    }
+
+    const theme = useTheme(getTheme());
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table data={tableData} theme={theme} layout={{ fixedHeader: true }}>
+        {(tableList: (ReversalRequest & { id: number; })[]) => (
+          <>
+            <Header>
+              <HeaderRow>
+                <HeaderCell>Name</HeaderCell>
+                <HeaderCell>Customer Location</HeaderCell>
+                <HeaderCell>Sign up date</HeaderCell>
+                <HeaderCell>Investment Date and Time</HeaderCell>
+                <HeaderCell>Refund Request Date and Time</HeaderCell>
+                <HeaderCell>Refund Request Approved</HeaderCell>
+              </HeaderRow>
+            </Header>
+
+            <Body>
+              {tableList.map((item:ReversalRequest & {id: number} , index: number) => (
+                <Row key={index} item={item}>
+                  <Cell>{item.name}</Cell>
+                  <Cell>{item.customerLocation}</Cell>
+                  <Cell>{format(item.signUpDate, 'dd MMMM yyyy')}</Cell>
+                  <Cell>{format(item.investmentDateTime, 'dd MMMM yyyy H:mm')}</Cell>
+                  <Cell>{format(item.refundRequestDateTime, 'dd MMMM yyyy H:mm')}</Cell>
+                  <Cell>{`${item.isReversalRequestApproved}`}</Cell>
+                </Row>
+              ))}
+            </Body>
+          </>
+        )}
+      </Table>
     </div>
   );
 }
---
+++ src/reversalRequests.ts
@@ -0,0 +1,62 @@
+export const reversalRequestsJSON = `[
+  {
+    "name": "Emma Smith",
+    "customerLocation": "US",
+    "signUpDate": "1/2/2020",
+    "requestSource": "phone",
+    "investmentDate": "1/2/2021",
+    "investmentTime": "06:00",
+    "refundRequestDate": "1/2/2021",
+    "refundRequestTime": "09:00"
+  },
+  {
+    "name": "Benjamin Johnson",
+    "customerLocation": "Europe",
+    "signUpDate": "12/2/2020",
+    "requestSource": "web app",
+    "investmentDate": "2/1/2021",
+    "investmentTime": "06:30",
+    "refundRequestDate": "1/2/2021",
+    "refundRequestTime": "23:00"
+  },
+  {
+    "name": "Olivia Davis",
+    "customerLocation": "Europe",
+    "signUpDate": "1/2/2020",
+    "requestSource": "web app",
+    "investmentDate": "2/2/2021",
+    "investmentTime": "13:00",
+    "refundRequestDate": "2/2/2021",
+    "refundRequestTime": "20:00"
+  },
+  {
+    "name": "Ethan Anderson",
+    "customerLocation": "US",
+    "signUpDate": "1/11/2011",
+    "requestSource": "web app",
+    "investmentDate": "2/1/2021",
+    "investmentTime": "13:00",
+    "refundRequestDate": "2/2/2021",
+    "refundRequestTime": "16:00"
+  },
+  {
+    "name": "Sophia Wilson",
+    "customerLocation": "US",
+    "signUpDate": "2/1/2020",
+    "requestSource": "phone",
+    "investmentDate": "2/1/2021",
+    "investmentTime": "22:00",
+    "refundRequestDate": "2/2/2021",
+    "refundRequestTime": "5:00"
+  },
+  {
+    "name": "Liam Martinez",
+    "customerLocation": "Europe",
+    "signUpDate": "1/1/2020",
+    "requestSource": "web app",
+    "investmentDate": "1/1/2021",
+    "investmentTime": "11:00",
+    "refundRequestDate": "11/1/2021",
+    "refundRequestTime": "12:00"
+  }
+]`;
